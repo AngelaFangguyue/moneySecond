037b49dd217f2c355a878bf0e95726de32cb9d1b
AngelaFang <1823476439@qq.com>
2020-06-08 08:44:18 +0800

    创建一个框架

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,50 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Home from '../views/Home.vue'
+import Money from '@/views/Money.vue';
+import Label from '@/views/Label.vue';
+import Statistics from '@/views/Statistics.vue';
+import NotFound from '@/views/NotFound.vue';
 
 Vue.use(VueRouter)
 
   const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: Home
+  // },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
+    {
+      path: '/',
+      name: 'Home',
+     // component: Home  //重定向
+      redirect: '/money'
+    },
+    {
+      path: '/money',
+      name: 'Money',
+      component: Money
+    },
+    {
+      path: '/label',
+      name: 'Label',
+      component: Label
+    },
+    {
+      path: '/statistics',
+      name: 'Statistics',
+      component: Statistics
+    },
+    {
+      path: '/*',
+      name: 'notFound',
+      component: NotFound
+    }
 ]
 
 const router = new VueRouter({
